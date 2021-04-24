4891b37597435cce44962e1957b2439eb6919b07
Panagiotis Iatrou <iatrou.panag@gmail.com>
2021-04-24 17:12:20 +0300

    Basic ground generation and player movement

---
+++ Ludum-Dare-48/Assets/Scripts/GroundManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundManager : MonoBehaviour
+{
+    public GameObject[] TilePrefabs;
+    public Transform TilesHolder;
+    private Dictionary<Vector2Int, Tile> groundTiles = new Dictionary<Vector2Int, Tile>();
+
+    private void Start()
+    {
+        GenerateInitialGround();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void GenerateInitialGround()
+    {
+        for (int i = -10; i <= 10; i++)
+        {
+            for (int j = -10; j <= -2; j++)
+            {
+                Vector2Int pos = new Vector2Int(i, j);
+                Vector3 worldPos = new Vector3(pos.x, pos.y, 0f);
+
+                GameObject tile;
+                // Generate only dirt on top 1 layer
+                if (j == -2)
+                {
+                    tile = Instantiate(TilePrefabs[0], worldPos, Quaternion.identity, TilesHolder);
+                }
+                else
+                {
+                    // 90% dirt 10% other ores
+                    int r1 = Random.Range(0, 10);
+                    if (r1 == 9)
+                    {
+                        int r2 = Random.Range(1, 2);
+                        tile = Instantiate(TilePrefabs[r2], worldPos, Quaternion.identity, TilesHolder);
+                    }
+                    else
+                    {
+                        tile = Instantiate(TilePrefabs[0], worldPos, Quaternion.identity, TilesHolder);
+                    }
+                }
+                groundTiles.Add(pos, new Tile(pos, tile));
+            }
+        }
+    }
+}
+
+public class Tile
+{
+    private Vector2Int position;
+    GameObject obj;
+
+    public Tile(Vector2Int position, GameObject obj)
+    {
+        this.position = position;
+    }
+}
---
+++ Ludum-Dare-48/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float forceSpeed = 1000f;
+    private float maxSpeed = 5f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-forceSpeed * Time.fixedDeltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(forceSpeed * Time.fixedDeltaTime, 0));
+        }
+
+        if (rb.velocity.magnitude >= maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
