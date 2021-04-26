e22608c5446e4830575ee7549f56162e5d361d0d
Panagiotis Iatrou <iatrou.panag@gmail.com>
2021-04-26 17:02:32 +0300

    Fixed background tiles + added cloud sprites + added more ores

--- Ludum-Dare-48/Assets/Scripts/GroundManager.cs
+++ Ludum-Dare-48/Assets/Scripts/GroundManager.cs
@@ -22,12 +22,15 @@ public class GroundManager : MonoBehaviour
 
     public Tilemap tilemap;
     public RuleTile RuleTile;
+    public Tile BackgroundTile;
     public Transform SpecialTilesHolder;
-    public GameObject OrePrefab;
+    public GameObject[] OrePrefabs;
     private GameObject playerGO;
     private Transform cameraTR;
     private Dictionary<Vector2Int, bool> minedTiles = new Dictionary<Vector2Int, bool>();
     private Dictionary<Vector2Int, GameObject> specialTiles = new Dictionary<Vector2Int, GameObject>();
+
+    private int[,] levelOresChances = { { 100, 0, 0, 0, 0 }, { 70, 100, 0, 0, 0 }, { 30, 70, 100, 0, 0 }, { 20, 40, 70, 100, 0 }, { 10, 30, 50, 70, 100 } };
 
     private void Start()
     {
@@ -56,17 +59,44 @@ public class GroundManager : MonoBehaviour
                         int r1 = Random.Range(0, 10);
                         if (r1 == 9)
                         {
-                            GameObject oreGO = Instantiate(OrePrefab, new Vector3(pos.x, pos.y, -1), Quaternion.identity, SpecialTilesHolder);
-                            int r2 = Random.Range(0, 5);
-                            if (r2 == 0)
-                            {
-                                oreGO.GetComponent<SpriteRenderer>().color = Color.yellow;
-                                oreGO.name = "Gold";
-                            }
+                            int level = -1;
+                            if (pos.y > -8)
+                                level = 0;
+                            else if (pos.y > -20)
+                                level = 1;
+                            else if (pos.y > -40)
+                                level = 2;
+                            else if (pos.y > -80)
+                                level = 3;
                             else
+                                level = 4;
+                            GameObject oreGO = Instantiate(OrePrefabs[Random.Range(0, OrePrefabs.Length)], new Vector3(pos.x, pos.y, -1), Quaternion.identity, SpecialTilesHolder);
+                            int r = Random.Range(0, 100);
+                            if (r <= levelOresChances[level, 0])
                             {
                                 oreGO.name = "Silver";
                             }
+                            else if (r <= levelOresChances[level, 1])
+                            {
+                                oreGO.name = "Gold";
+                                oreGO.GetComponent<SpriteRenderer>().color = Color.yellow;
+                            }
+                            else if (r <= levelOresChances[level, 2])
+                            {
+                                oreGO.name = "Emerald";
+                                oreGO.GetComponent<SpriteRenderer>().color = Color.green;
+                            }
+                            else if (r <= levelOresChances[level, 3])
+                            {
+                                oreGO.name = "Red Iron";
+                                oreGO.GetComponent<SpriteRenderer>().color = Color.red;
+                            }
+                            else if (r <= levelOresChances[level, 4])
+                            {
+                                oreGO.name = "Lapis";
+                                oreGO.GetComponent<SpriteRenderer>().color = Color.blue;
+                            }
+
                             specialTiles.Add(pos, oreGO);
                         }
                     }
@@ -84,7 +114,7 @@ public class GroundManager : MonoBehaviour
 
     public static void DestroyTileInPosition(Vector2Int position)
     {
-        Instance.tilemap.SetTile((Vector3Int)position, null);
+        Instance.tilemap.SetTile((Vector3Int)position, Instance.BackgroundTile);
         if (Instance.specialTiles.ContainsKey(position))
         {
             GameObject ore = Instance.specialTiles[position];
